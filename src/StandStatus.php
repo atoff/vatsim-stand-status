34e81829030e888ccf8d1bdf570fd43a8c98b8c5
Alex Toff <atoff@users.noreply.github.com>
2017-03-22 18:30:12 +0000

    Remove test pilot

--- src/StandStatus.php
+++ src/StandStatus.php
@@ -141,7 +141,7 @@ class StandStatus {
       $pilots = $vatsim->getPilots()->toArray();
 
       // INSERT TEST PILOTS
-      $pilots[] = array('callsign' => "TEST", "latitude" => 55.949228, "longitude" => -3.364303, "altitude" => 0, "groundspeed" => 0, "planned_destairport" => "TEST", "planned_depairport" => "TEST");
+      //$pilots[] = array('callsign' => "TEST", "latitude" => 55.949228, "longitude" => -3.364303, "altitude" => 0, "groundspeed" => 0, "planned_destairport" => "TEST", "planned_depairport" => "TEST");
 
       if(count($pilots) == 0){
         return false;
